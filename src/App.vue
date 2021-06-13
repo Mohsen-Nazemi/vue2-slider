e008a805077fb2f1522a7db5ad72eef44419cbd6
mohsen <mohsen.nazemi1376@gmail.com>
2021-06-13 15:58:56 +0430

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,87 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Slider
+      animation="fade"
+      v-model="sliderValue"
+      :duration="5000"
+      :speed="1000"
+    >
+      <SliderItem
+        v-for="(i, index) in list"
+        :key="index"
+        @click="changeIndex(1);"
+        :style="i"
+      >
+        <p style="line-height: 280px; font-size: 5rem; text-align: center;">
+          Page{{ index + 1 }}
+        </p>
+      </SliderItem>
+    </Slider>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import { Slider, SliderItem } from "vue-easy-slider";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
+    Slider,
+    SliderItem
+  },
+  data() {
+    return {
+      list: [],
+      sliderValue: 2
+    };
+  },
+  methods: {
+    changeIndex(index) {
+      this.sliderValue = index;
+    }
+  },
+  mounted() {
+    setTimeout(
+      () =>
+        (this.list = [
+          {
+            backgroundColor: "#3f51b5",
+            width: "100%",
+            height: "100%"
+          },
+          {
+            backgroundColor: "#eee",
+            width: "100%",
+            height: "100%"
+          },
+          {
+            backgroundColor: "#f44336",
+            width: "100%",
+            height: "100%"
+          },
+          {
+            backgroundColor: "#eee",
+            width: "100%",
+            height: "100%"
+          }
+        ]),
+      1000
+    );
   }
-}
+};
 </script>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+
+p {
+  margin: 0;
 }
 </style>
